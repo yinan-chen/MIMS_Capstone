6de5618393b75169c24fca5ac2ee420faf4558a2
Victor-Hsin <victor_hsin@berkeley.edu>
2021-04-24 22:01:40 -0400

    added countdown timer for the pre-existing session card

---
+++ js/timer.js
@@ -0,0 +1,23 @@
+var countDownTimeInSec = 60 * 5;
+
+function startTimer(duration, display) {
+    var timer = duration, minutes, seconds;
+    refreshIntervalId = setInterval(function () {
+        minutes = parseInt(timer / 60, 10);
+        seconds = parseInt(timer % 60, 10);
+
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        display.textContent = minutes + ":" + seconds;
+
+        if (--timer < 0) {
+            clearInterval(refreshIntervalId);
+        }
+    }, 1000);
+}
+
+window.onload = function () {
+    display = document.querySelector('#time');
+    startTimer(countDownTimeInSec, display);
+};
