b8e9b8240bba1cd7e57ef4155bfa4cb72cf609d6
Victor-Hsin <victor_hsin@berkeley.edu>
2021-04-27 11:39:03 -0400

    added private session card

--- js/main.js
+++ js/main.js
@@ -18,11 +18,10 @@ function createNewSessionHTML(topic, description, privateSwitch) {
                                 "<div class='col-md-6'>" +
                                     "<div class='card-body left-card'>";
 
-    session_str += "<h6 >Session Info</h2>";
+    session_str += privateSwitch == false ? "<h6 >Session Info</h2>": "<h6 >Private Session Info</h2>";
     session_str += "<h5 class='card-title'>" + topic + "</h5>";
     session_str += "<h6>Description</h6>";
     session_str += "<p class='card-text'>" + description + "</p>";
-    session_str += "<p class='card-text'><small class='text-muted'>" + "Private Switch: " + privateSwitch + "</small></p>";
 
     session_str += "</div></div>" +
                         "<div class='col-md-6'>" +
@@ -33,11 +32,28 @@ function createNewSessionHTML(topic, description, privateSwitch) {
                                     `<p class="card-text"><small class="text-muted">Ends in <span>--:--</span></small></p>` +
                                 `</div>` +
                                 "<p class='card-text'></p>" +
-                                "<p class='card-text'><small class=\"text-muted\">Last updated 3 hours ago</small></p>" +
-                                `<div class="d-grid gap-2 col-1.5 mx-auto join-btn">` + 
+                                `<div class="sessionMembers">
+                                     <img class="rounded-circle sessionMembersPicture" src="./img/profilePicture.png">
+                                     <img class="rounded-circle sessionMembersPicture" src="./img/profilePicture.png">
+                                     <img class="rounded-circle sessionMembersPicture" src="./img/profilePicture.png">
+                                     <br>
+                                     <img class="rounded-circle sessionMembersPicture" src="./img/profilePicture.png">
+                                     <img class="rounded-circle sessionMembersPicture" src="./img/profilePicture.png">
+                                     <img class="rounded-circle sessionMembersPicture" src="./img/moreProfilePicture.png">
+                                 </div>`;
+    if (privateSwitch == false){
+        session_str +=          `<div class="d-grid gap-2 col-1.5 mx-auto join-btn">` + 
                                     `<button class="btn btn-primary" type="button" data-bs-toggle="modal" data-bs-target="#joinFormModal">Join</button>` +
-                                `</div>` +
-                            "</div></div></div></div>";
+                                `</div>`;
+    }                       
+    else {
+        session_str +=          `<div class="opaqueLayer">
+                                 </div>
+                                 <div class="d-grid gap-2 col-1.5 mx-auto askPermission">
+                                    <button class="btn btn-primary" type="button" data-bs-toggle="modal" data-bs-target="#joinFormModal">Ask for permission</button>
+                                 </div>`;
+    }
+    session_str += "</div></div></div></div>";
 
     return session_str;
 }
