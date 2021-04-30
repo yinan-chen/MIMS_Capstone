9e098ee3a100191597e7257b9d6c2fc127463e61
Victor-Hsin <victor_hsin@berkeley.edu>
2021-04-30 18:40:12 -0400

    added timer feature for private session

--- js/timer.js
+++ js/timer.js
@@ -1,8 +1,26 @@
 var countDownTimeInSec = 60 * 10;
+var privateCountDownTimeInSec = 60 * 45;
 
-function startTimer(duration, display) {
+function startPublicTimer(duration, display) {
     var timer = duration, minutes, seconds;
-    refreshIntervalId = setInterval(function () {
+    var refreshIntervalId = setInterval(function () {
+        minutes = parseInt(timer / 60, 10);
+        seconds = parseInt(timer % 60, 10);
+
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        display.textContent = minutes + ":" + seconds;
+
+        if (--timer < 0) {
+            clearInterval(refreshIntervalId);
+        }
+    }, 1000);
+}
+
+function startPrivateTimer(duration, display) {
+    var timer = duration, minutes, seconds;
+    var refreshIntervalId = setInterval(function () {
         minutes = parseInt(timer / 60, 10);
         seconds = parseInt(timer % 60, 10);
 
@@ -18,6 +36,8 @@ function startTimer(duration, display) {
 }
 
 window.onload = function () {
-    display = document.querySelector('#time');
-    startTimer(countDownTimeInSec, display);
+    publicDisplay = document.querySelector('#time');
+    startPublicTimer(countDownTimeInSec, publicDisplay);
+    privateDisplay = document.querySelector('#privateTime');
+    startPrivateTimer(privateCountDownTimeInSec, privateDisplay)
 };
