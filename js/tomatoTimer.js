a5f7c89030c1b80c213d2866cb6c44c628eb78ee
Victor-Hsin <victor_hsin@berkeley.edu>
2021-05-04 10:25:32 -0400

    added timer to tomatoTimer and changed the progress bar accordingly when the time countdowns

---
+++ js/tomatoTimer.js
@@ -0,0 +1,27 @@
+var countDownTimeInSec = 60 * 15;
+
+function startPublicTimer(duration, display) {
+    var timer = duration, minutes, seconds;
+    var refreshIntervalId = setInterval(function () {
+        minutes = parseInt(timer / 60, 10);
+        seconds = parseInt(timer % 60, 10);
+
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        display.textContent = minutes + ":" + seconds;
+        var progressBar1 = document.querySelector("#progressBar1");
+        var width = 100 * (timer / (25*60));
+        var widthStr = width.toString();
+        var style = "width: " + widthStr + "%;";
+        progressBar1.setAttribute("style", style)
+        if (--timer < 0) {
+            clearInterval(refreshIntervalId);
+        }
+    }, 1000);
+}
+
+window.onload = function () {
+    publicDisplay = document.querySelector('#tomatoTimer1');
+    startPublicTimer(countDownTimeInSec, publicDisplay);
+};
